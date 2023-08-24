72220840a8616e4baab5ccf49f619408de0e55f6
Rukhti <rukhti_25@hotmail.com>
2023-08-24 19:26:22 +0500

    Update sendTextMessage.py

--- examples/sendTextMessage.py
+++ examples/sendTextMessage.py
@@ -1,12 +1,12 @@
 from whatsapp_api_client_python import API as API
 
-ID_INSTANCE = '1101000001'
-API_TOKEN_INSTANCE = '3e03ea9ff3324e228ae3dfdf4d48e409bfa1b1ad0b0c46bf8c'
+ID_INSTANCE = '1101824950'
+API_TOKEN_INSTANCE = 'd527a190b4a441e08687ed6ac7d658725edfe214649b4cb49d'
 
 greenAPI = API.GreenApi(ID_INSTANCE, API_TOKEN_INSTANCE)
 
 def main():
-    result = greenAPI.sending.sendMessage('11001234567@c.us', 'Message text')
+    result = greenAPI.sending.sendMessage('923479811551@c.us', 'G')
     print(result.data)
 
 if __name__ == "__main__":
